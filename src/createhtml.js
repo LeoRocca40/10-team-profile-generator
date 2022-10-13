3f2dee983445f264ede8860c0a135b558490b9fd
97ToyoTaco <leocubshat@gmail.com>
2022-10-12 21:50:53 -0700

    first commit, made all files and parts of the code for index.js and createhtml.js

---
+++ src/createhtml.js
@@ -0,0 +1,25 @@
+const produceTeam = (userTeam) => {
+    return
+    `<!DOCTYPE html>
+    <html lang="en-US">
+    
+      <head>
+        <meta charset="UTF-8">
+        <title>Student Bio</title>
+        bootstrap and css
+      </head>
+    
+      <body>
+        <div>${userTeam}<div>
+      </body>
+    
+    </html>
+    `
+}
+
+const produceManager = (managerInfo) => {
+    return
+    `
+    <div> Info and shit <div>
+    `
+}
